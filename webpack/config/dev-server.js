d2a8b8c59b61d9fa5a3a92ef33084f195808246f
Adam Richardson <adam.rickardson@gmail.com>
2017-06-12 20:42:25 +0100

    webpack minor improvements

--- webpack/config/dev-server.js
+++ webpack/config/dev-server.js
@@ -1,19 +1,19 @@
 // separate configuration for 'development'
 
-var WebpackDevServer = require('webpack-dev-server');
+var path = require('path'); // this is a node specific package
 var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.default');
-var path = require('path');
 
 var compiler = webpack(config);
 
 var server = new WebpackDevServer(compiler, {
-  hot: true, // module replacement
-  filename: config.output.filename,
-  publicPath: config.output.publicPath,
-  stats: {
-    color: true
-  }
+	hot: true, // module replacement
+	filename: config.output.filename,
+	publicPath: config.output.publicPath,
+	stats: {
+		color: true
+	}
 });
 
 server.listen(8080, 'localhost');
